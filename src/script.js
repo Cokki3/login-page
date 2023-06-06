02fdc1495243a14926e7fcf1f34c3552e4c290e9
Cokki3 <135257458+Cokki3@users.noreply.github.com>
2023-06-06 14:39:44 -0300

    update

---
+++ src/script.js
@@ -0,0 +1,47 @@
+let email = document.querySelector('#email')
+let senha = document.querySelector('#senha')
+let alerta = document.querySelector('#alerta')
+let login = document.querySelector('#login')
+
+let e;
+let s;
+
+const showFlash = (cor) => {
+    alerta.style.color = cor
+}
+const hideFlash = () => {
+    alerta.style.color = 'transparent'
+}
+
+login.addEventListener('click', (event) => {
+    event.preventDefault()
+    e = email.value
+    s = senha.value
+
+    if (e.length == 0 && s.length == 0) {
+        alerta.innerHTML = 'Dados Incorretos!'
+        showFlash('red')
+        setTimeout(hideFlash, 1000);
+    }
+    else if (validateEmail(e) == false || e.length == 0) {
+        alerta.innerHTML = 'Email incorreto!'
+        showFlash('red')
+        setTimeout(hideFlash, 1000);
+        
+    }
+    else if (s.length == 0 || s.length < 8) {
+        alerta.innerHTML = 'Senha curta!'
+        showFlash('red')
+        setTimeout(hideFlash, 1000);
+    }
+    else {
+        email.value = ''
+        senha.value = ''
+    }
+
+})
+
+function validateEmail(email) {
+    var re = /\S+@\S+\.\S+/;
+    return re.test(email);
+}
